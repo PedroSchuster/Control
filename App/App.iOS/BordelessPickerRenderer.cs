36164b47535d3679910edf249f1c38062548d306
Pedro Schuster <pedro.ssc42@gmail.com>
2023-01-06 11:05:11 -0300

    Project fixes

---
+++ App/App.iOS/BordelessPickerRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace App.iOS
+{
+    public class BorderlessPickerRenderer : PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                return;
+            }
+
+            Control.Layer.BorderWidth = 0;
+            Control.BorderStyle = UITextBorderStyle.None;
+        }
+
+    }
+}
